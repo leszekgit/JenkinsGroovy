563df47c683fea636434a63df58fb42d40211b6d
leszekgit <lexgzk123@gmail.com>
2020-09-04 13:23:36 -0500

    Update

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("I love Groovy!");
+            Console.WriteLine("I love Groovy!!");
         }
     }
 }
